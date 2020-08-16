77dbaac8528408a746e6df7154ceb9f8250c3fce
cindyhuang19 <cindyhuang19@outlook.com>
2020-08-16 10:58:45 -0400

    navigation to glossary

--- src/app/home/home-router.ts
+++ src/app/home/home-router.ts
@@ -6,6 +6,8 @@ import { HomePageComponent } from './modules/home-page/home-page.component';
 import { HomePageMainComponent } from './modules/home-page-main/home-page-main.component';
 import { ManageExpensesComponent } from './modules/manage-expenses/manage-expenses.component';
 import { ManageGoalsComponent } from './modules/manage-goals/manage-goals.component';
+import { AchievementsComponent } from './modules/achievements/achievements.component';
+import { GlossaryComponent } from './modules/glossary/glossary.component';
 
 const routes: Routes = [
     {
@@ -39,6 +41,20 @@ const routes: Routes = [
                         component: ManageGoalsComponent,
                         data: {
                             pageName: 'manage-goals'
+                        }
+                    },
+                    {
+                        path: 'glossary',
+                        component: GlossaryComponent,
+                        data: {
+                            pageName: 'glossary'
+                        }
+                    },
+                    {
+                        path: 'achievements',
+                        component: AchievementsComponent,
+                        data: {
+                            pageName: 'achievements'
                         }
                     },
                 ]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,8 @@ import { HomePageMainComponent } from './modules/home-page-main/home-page-main.c
 import { ManageExpensesComponent} from './modules/manage-expenses/manage-expenses.component';
 import { ManageGoalsComponent} from './modules/manage-goals/manage-goals.component';
 import { MDBModalRef, MDBModalService } from 'ng-uikit-pro-standard';
+import { GlossaryComponent} from './modules/glossary/glossary.component';
+import { AchievementsComponent} from './modules/achievements/achievements.component';
 
 
 import { NgModule } from '@angular/core';
@@ -13,7 +15,8 @@ import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
-        HomeComponent, HomePageComponent, HomePageMainComponent, ManageExpensesComponent, ManageGoalsComponent
+        HomeComponent, HomePageComponent, HomePageMainComponent, ManageExpensesComponent, ManageGoalsComponent,
+        GlossaryComponent, AchievementsComponent
     ],
     imports: [HomeRouter, FormsModule, CommonModule],
     exports: [HomeComponent],
